2af77b0389fc3684593c30110fe4004e6cf736ef
steank <dnd.initiative.tracker@gmail.com>
2024-03-11 17:27:32 -0700

    Disable "lack of support" for public bots and verified bots

--- src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
+++ src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
@@ -27,8 +27,6 @@ import java.nio.file.Paths;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.ApplicationInfo;
-import net.dv8tion.jda.api.entities.User;
 import okhttp3.*;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -218,13 +216,6 @@ public class OtherUtil
      */
     public static String getUnsupportedBotReason(JDA jda) 
     {
-        if (jda.getSelfUser().getFlags().contains(User.UserFlag.VERIFIED_BOT))
-            return "The bot is verified. Using JMusicBot in a verified bot is not supported.";
-
-        ApplicationInfo info = jda.retrieveApplicationInfo().complete();
-        if (info.isBotPublic())
-            return "\"Public Bot\" is enabled. Using JMusicBot as a public bot is not supported. Please disable it in the Developer Dashboard.";
-
         return null;
     }
 }
